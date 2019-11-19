dfd748098f4cd6ba12befd8e6e887c404b12a664
wtildestar <31348327+wtildestar@users.noreply.github.com>
2019-11-19 14:09:29 +0500

    WeatherIconManager to determine the desired icon

---
+++ APIUsage/CurrentWeather.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentWeather.swift
+//  APIUsage
+//
+//  Created by wtildestar on 19/11/2019.
+//  Copyright © 2019 wtildestar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CurrentWeather {
+    let temperature: Double
+    let appearentTemperature: Double
+    let humidity: Double
+    let plessure: Double
+    let icon: UIImage
+}
--- APIUsage/ViewController.swift
+++ APIUsage/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var TemperatureLabel: UILabel!
+    @IBOutlet weak var appearentTemperatureLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBAction func refreshButtonTapped(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
