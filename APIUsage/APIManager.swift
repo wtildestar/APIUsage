2ef40fa0ea0404e84c0e57ab4fdb1c5a499a4277
wtildestar <31348327+wtildestar@users.noreply.github.com>
2019-11-19 21:54:13 +0500

    Handler structure interface

--- APIUsage/APIManager.swift
+++ APIUsage/APIManager.swift
@@ -11,11 +11,17 @@ import Foundation
 typealias JSONTask = URLSessionDataTask
 typealias JSONCompletionHandler = ([String: AnyObject]?, HTTPURLResponse?, Error?) -> Void
 
+enum APIResult<T> {
+    case Success(T)
+    case Failure(Error)
+}
+
 protocol APIManager {
     var sessionConfiguration: URLSessionConfiguration { get }
     var session: URLSession { get }
-    
+    // пишем интерфейс
     func JSONTaskWith(request: URLRequest, completionHandler: JSONCompletionHandler) -> JSONTask
+    func fetch<T>(request: URLRequest, parse: ([String: AnyObject]) -> T?, completionHandler: (APIResult<T>) -> Void)
     
     // можно указать не .default сессию а свою
     init(sessionConfiguration: URLSessionConfiguration)
